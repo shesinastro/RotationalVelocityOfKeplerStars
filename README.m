cd1e7a17b66eef75aac2005a0cf04a21dcf5bf58
Serena Repetto <repetto.serena@gmail.com>
2017-08-22 12:14:47 +0300

    Update and rename README to README.m

---
+++ README.m
@@ -0,0 +1,8 @@
+*** Script to download html tables of KOI stellar properties from the ExoFOP catalogue and extract the value of vsini. <br />
+
+Execute as: <br />
+
+1) $ ./download.sh  <br />
+2) $ /.ExtractVrot.sh   <br />
+
+Input: list of KOIs of interest; either all KOIs, or only KOI with high-signal-to-noise spectra.
